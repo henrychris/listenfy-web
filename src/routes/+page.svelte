75435db68c6478d3b949a36f64ba8ee94967a29c
henrychris <64085210+henrychris@users.noreply.github.com>
2026-02-19 11:32:13 +0100

    move header and footer to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import Features from '$lib/components/Features.svelte';
 	import HowItWorks from '$lib/components/HowItWorks.svelte';
 	import Commands from '$lib/components/Commands.svelte';
-	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <svelte:head>
@@ -15,11 +13,9 @@
 	/>
 </svelte:head>
 
-<Header />
 <main class="flex flex-col gap-4">
 	<Hero />
 	<Features />
 	<HowItWorks />
 	<Commands />
 </main>
-<Footer />
