c2898568a03ee3857f9326d759ba7cd703250d2d
henrychris <64085210+henrychris@users.noreply.github.com>
2026-02-26 15:27:37 +0100

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/Header.svelte';
+	import Header from '$lib/components/Header.svelte';import favicon from '$lib/assets/favicon.ico';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
-
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <Header />
 {@render children()}
 <Footer />
