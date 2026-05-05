5b7f191f64256f5592c46a11daaf8035656c0c6b
henrychris <64085210+henrychris@users.noreply.github.com>
2026-05-05 23:35:37 +0100

    use posthog reverse proxy

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,16 @@
 import posthog from 'posthog-js';
 import { browser, dev } from '$app/environment';
-import { PUBLIC_POSTHOG_KEY } from '$env/static/public';
+import {
+	PUBLIC_POSTHOG_KEY,
+	PUBLIC_POSTHOG_HOST,
+	PUBLIC_POSTHOG_UI_HOST
+} from '$env/static/public';
 
 export const load = async () => {
 	if (browser && !dev) {
 		posthog.init(PUBLIC_POSTHOG_KEY, {
-			api_host: 'https://us.i.posthog.com',
+			api_host: PUBLIC_POSTHOG_HOST,
+			ui_host: PUBLIC_POSTHOG_UI_HOST,
 			defaults: '2026-01-30'
 		});
 	}
